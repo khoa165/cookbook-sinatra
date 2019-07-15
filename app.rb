06def74687084de931995623485c312d3ac4cf64
Khoa Le <khoatle165@gmail.com>
2019-07-15 15:18:25 +0200

    sinatra 101

--- app.rb
+++ app.rb
@@ -8,5 +8,14 @@ configure :development do
 end
 
 get '/' do
-  'Hi Khoa!'
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
